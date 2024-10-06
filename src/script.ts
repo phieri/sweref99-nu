27ea60f39205ae73f562aa4d09f48079b2abe7b2
Philip Eriksson <12006381+phieri@users.noreply.github.com>
2024-10-06 11:40:08 +0000

    Early dev

--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,23 @@
 document.addEventListener(
-    "keydown",
-    (event) => {
-      if (event.key === "F1") {
-        document.location = "https://sweref99.nu/om.html";
-      }
-    },
-    false,
-  );
+  "keydown",
+  (event) => {
+    if (event.key === "F1") {
+      document.location = "https://sweref99.nu/om.html";
+    }
+  },
+  false,
+);
+
+const shareData = {
+  title: "Position",
+  text: "SWEREF 99 TM: N E"
+};
+
+const btn = document.querySelector("#share-btn");
+btn.addEventListener("click", async () => {
+  try {
+    await navigator.share(shareData);
+  } catch (err) {
+    alert("Kunde inte dela.");
+  }
+});
